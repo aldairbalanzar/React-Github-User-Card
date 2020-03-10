7aea0d7f43bf4ba7149a35212273e5b607b2d305
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-03-10 18:57:21 -0400

    updated files to mvp

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -26,39 +26,31 @@ class App extends Component {
       axios
       .get('https:api.github.com/users/aldairbalanzar/followers')
       .then(res => {
-        console.log('followers res: ', res);
-        this.setState({
-          followersList: res.data.map(item => {
+        console.log('followers res: ', res.data);
+          res.data.map(item => {
             axios.get(item.url)
             .then(res => {
-              console.log('item res: ', res.data);
+              this.setState({
+                followersList: [...this.state.followersList, res.data]
+              })
             })
           })
-        })
       })
   }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   axios
-  //   .get('https:api.github.com/users/aldairbalanzar/followers')
-  //   .then(res => {
-  //     console.log('followers res: ', res.data);
-  //     res.data.map(item => {
-  //       axios.get(item.url)
-  //       .then(res => {
-  //         console.log('res: ', res.data);
-  //         // this.setState({
-  //         //   userList: res.data
-  //         // })
-  //       });
-  //     })
-  //   })
-  // }
+  componentDidUpdate() {
+    console.log(this.state.followersList)
+  }
 
   render(){
     return(
       <div>
         <p>{this.state.user.login}</p>
+        {this.state.followersList.map(item => {
+          return(
+          <p>{item.login}</p>
+          )
+        })}
       </div>
     )
   }
